4a666fbc14a90d411096e8b479f62e6cf9c5d425
LeeDongHo <edh1021@gmail.com>
2021-02-27 19:13:40 +0900

    ignore update

--- python/django_1/project/maps/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from maps.models import Question, Choice
-
-# admin.site에 등록
-admin.site.register(Question)
-admin.site.register(Choice)
-# Register your models here.
--- python/kko_api/test_client.py
+++
@@ -1,9 +0,0 @@
-from http.client import HTTPConnection
-
-host = 'localhost'
-conn = HTTPConnection(host)
-conn.request('GET','/')
-r1 = conn.getresponse()
-print(r1.status,r1.reason)
-
-data1 = r1.read()
