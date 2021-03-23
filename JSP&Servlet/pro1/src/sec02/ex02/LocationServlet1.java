d5a7f3569db5a0406281a4d77e4fc01c9958febc
LeeDongHo <edh1021@gmail.com>
2021-03-23 10:16:00 +0900

    forwarding - location

---
+++ JSP&Servlet/pro1/src/sec02/ex02/LocationServlet1.java
@@ -0,0 +1,24 @@
+package sec02.ex02;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LocationServlet1", value = "/location1")
+public class LocationServlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.print("<script type='text/javascript'>");
+        pw.print("location.href='/location2';");
+        pw.print("</script>");
+    }
+}
---
+++ JSP&Servlet/pro1/src/sec02/ex02/LocationServlet2.java
@@ -0,0 +1,22 @@
+package sec02.ex02;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LocationServlet2", value="/location2")
+public class LocationServlet2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.print("<h2>Location을 이용한 redirect 실습</h2>");
+    }
+}
