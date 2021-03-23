0eff9b4a5c7908dd9eb723647a2c762dd70da403
LeeDongHo <edh1021@gmail.com>
2021-03-23 10:16:22 +0900

    forwarding - redirect

---
+++ JSP&Servlet/pro1/src/sec02/ex02/RedirectServlet2.java
@@ -0,0 +1,24 @@
+package sec02.ex02;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "RedirectServlet2", value = "/redirect2")
+public class RedirectServlet2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        String name = request.getParameter("name");
+        pw.print("Name : " + name);
+    }
+}
