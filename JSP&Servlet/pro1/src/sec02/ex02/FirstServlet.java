dc02515f6df83dfe91fb4e28eba5807b41466187
LeeDongHo <edh1021@gmail.com>
2021-03-23 10:16:42 +0900

    forwarding - refresh

---
+++ JSP&Servlet/pro1/src/sec02/ex02/FirstServlet.java
@@ -0,0 +1,23 @@
+package sec02.ex02;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/refresh1")
+public class FirstServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        // 1초후 서블릿 second로 재요청
+        response.addHeader("Refresh","1;url=refresh2");
+    }
+}
---
+++ JSP&Servlet/pro1/src/sec02/ex02/SecondServlet.java
@@ -0,0 +1,22 @@
+package sec02.ex02;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/refresh2")
+public class SecondServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("<h2>Refresh를 이용한 redirect 실습</h2>");
+    }
+}
