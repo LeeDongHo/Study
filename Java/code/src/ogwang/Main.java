bf54e8f662eaef3c79bb6b3e753927678500bbfe
LeeDongHo <edh1021@gmail.com>
2021-04-29 17:03:44 +0900

    start

---
+++ Java/code/src/ogwang/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JDBCTest jdbc = new JDBCTest();
+        List<HashMap<String,Object>> memoList = jdbc.testSelect();
+        System.out.println("==================================");
+        System.out.println("no \t name \t msg \t writeday");
+        System.out.println("==================================");
+        if(memoList != null) {
+            for(HashMap<String,Object> memo : memoList) {
+                System.out.println(memo.get("NO")+"\t"+memo.get("NAME")+"\t"+memo.get("MSG")+"\t"+memo.get("WRITEDATE"));
+            }
+        }
+        else
+            System.out.println("== 입력된 데이터가 없습니다 ==");
+    }
+}
+
+
+
